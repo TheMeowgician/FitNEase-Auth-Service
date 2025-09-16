14c12aead28a1dd82b66cdc7f615f5668249b49f
TheMeowgician <gabsmaglaya@gmail.com>
2025-09-16 10:00:42 +0800

    added fields for role permissions migration file

--- database/migrations/2025_09_16_015354_create_role_permissions_table.php
+++ database/migrations/2025_09_16_015354_create_role_permissions_table.php
@@ -12,8 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('role_permissions', function (Blueprint $table) {
-            $table->id();
+            $table->id('role_permission_id');
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('permission_id');
+            $table->timestamp('assigned_at')->default(now());
+            $table->unsignedBigInteger('assigned_by')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
+
+            $table->foreign('role_id')->references('role_id')->on('roles')->onDelete('cascade');
+            $table->foreign('permission_id')->references('permission_id')->on('permissions')->onDelete('cascade');
+            $table->foreign('assigned_by')->references('user_id')->on('users')->onDelete('set null');
+            $table->unique(['role_id', 'permission_id']);
         });
     }
 
