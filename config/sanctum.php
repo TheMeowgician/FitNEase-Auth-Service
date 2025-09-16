b185c6988996b6a0e608b6a21a8805c3fab11aec
TheMeowgician <gabsmaglaya@gmail.com>
2025-09-16 10:14:25 +0800

    Configured sanctum settings

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,12 +15,15 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
-    ))),
+    'stateful' => [
+        'localhost',
+        'localhost:3000',
+        'localhost:8080',
+        '127.0.0.1',
+        '127.0.0.1:8000',
+        '::1',
+        'fitnease.local',
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -47,7 +50,7 @@ return [
     |
     */
 
-    'expiration' => null,
+    'expiration' => 525600, // 1 year in minutes for mobile apps
 
     /*
     |--------------------------------------------------------------------------
